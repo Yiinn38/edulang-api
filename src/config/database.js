d5371e299b910c28f8490f76e91606d4ed8a6d68
Cesar Romero <rasec.romero46@gmail.com>
2025-06-21 10:07:17 -0600

    v1 bugfix

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,20 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  port: process.env.DB_PORT || 3306,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306,
+    logging: false,
+    retry: {
+      max: 5,
+      timeout: 5000
+    }
+  }
+);
 
 module.exports = sequelize;
---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const User = require("./User");
+const Student = require("./Student");
+const Teacher = require("./Teacher");
+const Course = require("./Course");
+const Enrollment = require("./Enrollment");
+const Grade = require("./Grade");
+const Payment = require("./Payment");
+
+module.exports = {
+  User,
+  Student,
+  Teacher,
+  Course,
+  Enrollment,
+  Grade,
+  Payment,
+};
